c6bc96d9c7770a8294684f9f463ee1aa92835e29
Phil Thomas <pthomas551@gmail.com>
2016-03-26 18:54:25 -0500

    add trending

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,17 +1,29 @@
 angular.module('unicorn.controllers', [])
 
-.controller('UnicornsCtrl', function($scope, $timeout, PictureService) {
+.controller('UnicornsCtrl', function($scope, $timeout, UnicornService) {
 	$scope.items = [];
-	PictureService.GetFeed().then(function(items){
+	UnicornService.GetFeed().then(function(items){
 		$scope.items = items;
 	});
 	$scope.loadMore = function() {
-		PictureService.GetNewImages().then(function(items){
+		UnicornService.GetNewImages().then(function(items){
 			$scope.items = $scope.items.concat(items);
 			$scope.$broadcast('scroll.infiniteScrollComplete');
 		});
 	};
 })
-.controller('TrendingCtrl', function() {})
+.controller('TrendingCtrl', function($scope, $timeout, TrendingService) {
+	$scope.items = [];
+	TrendingService.GetFeed().then(function(items){
+		$scope.items = items;
+	});
+	$scope.loadMore = function() {
+		TrendingService.GetNewImages().then(function(items){
+			$scope.items = $scope.items.concat(items);
+			$scope.$broadcast('scroll.infiniteScrollComplete');
+		});
+	};
+})
 .controller('UnicornDetailCtrl', function() {})
-.controller('SearchCtrl', function() {});
+.controller('SearchCtrl', function() {})
+;
--- www/js/services.js
+++ www/js/services.js
@@ -1,22 +1,43 @@
 angular.module('unicorn.services', [])
 
-.factory('PictureService', function($http) {
-  var GIPHY_URL = 'http://api.giphy.com/v1/gifs/search?q=unicorn&api_key=dc6zaTOxFJmzC&rating=pg';
+.factory('UnicornService', function($http) {
+  var UNICORN_URL = 'http://api.giphy.com/v1/gifs/search?q=unicorn&api_key=dc6zaTOxFJmzC&rating=pg';
   var items = [];
   var offset = 25;
   return {
     GetFeed: function() {
-      return $http.get(GIPHY_URL).then(function(response) {
+      return $http.get(UNICORN_URL).then(function(response) {
         items = response.data.data;
         return items;
       });
     },
     GetNewImages: function() {
-      return $http.get(GIPHY_URL+'&offset='+offset).then(function(response) {
+      return $http.get(UNICORN_URL+'&offset='+offset).then(function(response) {
         items = response.data.data;
         offset += 25;
         return items;
       });
     }
   };
-});
+})
+.factory('TrendingService', function($http) {
+  var TRENDING_URL = 'http://api.giphy.com/v1/gifs/trending?&api_key=dc6zaTOxFJmzC&rating=pg';
+  var items = [];
+  var offset = 25;
+  return {
+    GetFeed: function() {
+      return $http.get(TRENDING_URL).then(function(response) {
+        items = response.data.data;
+        return items;
+      });
+    },
+    GetNewImages: function() {
+      return $http.get(TRENDING_URL+'&offset='+offset).then(function(response) {
+        items = response.data.data;
+        offset += 25;
+        return items;
+      });
+    }
+  };
+})
+;
