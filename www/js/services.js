535f36ca908a1dc8043fdeb49832d97a569bc2f2
Phil Thomas <pthomas551@gmail.com>
2016-03-26 18:35:00 -0500

    finish first release

--- www/js/services.js
+++ www/js/services.js
@@ -1,8 +1,9 @@
 angular.module('unicorn.services', [])
 
 .factory('PictureService', function($http) {
-  var GIPHY_URL = 'http://api.giphy.com/v1/gifs/search?q=unicorn&api_key=dc6zaTOxFJmzC   ';
+  var GIPHY_URL = 'http://api.giphy.com/v1/gifs/search?q=unicorn&api_key=dc6zaTOxFJmzC&rating=pg';
   var items = [];
+  var offset = 25;
   return {
     GetFeed: function() {
       return $http.get(GIPHY_URL).then(function(response) {
@@ -11,8 +12,9 @@ angular.module('unicorn.services', [])
       });
     },
     GetNewImages: function() {
-      return $http.get(GIPHY_URL).then(function(response) {
+      return $http.get(GIPHY_URL+'&offset='+offset).then(function(response) {
         items = response.data.data;
+        offset += 25;
         return items;
       });
     }
